0379b62a6185b31d221206da27dec8fb70efcb6b
Ray Zhang <1789958884@qq.com>
2024-03-22 10:14:34 +0800

    initial

---
+++ RyRegularExpresson.cpp
@@ -0,0 +1,130 @@
+﻿/**
+ * @author rayzhang
+ * @brief
+ * @date 2024-03-19
+ */
+#include "RyRegularExpresson.h"
+
+#include <iostream>
+#include <ostream>
+
+namespace ry
+{
+RyRegularExpressionMatch::RyRegularExpressionMatch(const std::smatch& matches)
+    : m_matches(matches)
+{ }
+
+size_t RyRegularExpressionMatch::count() const
+{
+    return m_matches.size();
+}
+
+std::string RyRegularExpressionMatch::at(size_t index) const
+{
+    return m_matches[index].str();
+}
+
+std::string RyRegularExpressionMatch::captured(size_t index) const
+{
+    return at(index);
+}
+
+std::string RyRegularExpressionMatch::operator[](size_t index) const
+{
+    return m_matches[index].str();
+}
+
+std::vector<std::string> RyRegularExpressionMatch::capturedTexts() const
+{
+    std::vector<std::string> capturedTexts;
+
+    // 从1开始，因为matches[0]包含的是整个匹配的字符串
+    for (size_t i = 1; i < m_matches.size(); ++i) {
+        // 添加捕获的文本到结果vector中
+        capturedTexts.push_back(m_matches[i].str());
+    }
+
+    return capturedTexts;
+}
+
+bool RyRegularExpressionMatch::hasMatch() const
+{
+    return !m_matches.empty();
+}
+
+RyRegularExpression::RyRegularExpression(const std::string& pattern, int flags)
+    : m_regex(pattern, static_cast<std::regex_constants::syntax_option_type>(flags) | std::regex_constants::icase)
+{ }
+
+void RyRegularExpression::setPattern(const std::string& pattern)
+{
+    m_regex = std::regex(pattern, m_regex.flags());
+}
+
+RyRegularExpressionMatch RyRegularExpression::match(const std::string& text) const
+{
+    std::smatch matches;
+    try {
+        std::regex_match(text, matches, m_regex);
+    }
+    catch (...) {
+        handleException();
+    }
+    return RyRegularExpressionMatch(matches);
+}
+
+RyRegularExpressionMatch RyRegularExpression::search(const std::string& text) const
+{
+    std::smatch matches;
+    try {
+        std::regex_search(text, matches, m_regex);
+    }
+    catch (...) {
+        handleException();
+    }
+    return RyRegularExpressionMatch(matches);
+}
+
+std::string RyRegularExpression::replace(const std::string& text, const std::string& replacement) const
+{
+    try {
+        return std::regex_replace(text, m_regex, replacement);
+    }
+    catch (...) {
+        handleException();
+    }
+    return text;
+}
+
+std::vector<std::string> RyRegularExpression::split(const std::string& text) const
+{
+    try {
+        return std::vector<std::string>{std::sregex_token_iterator(text.begin(), text.end(), m_regex, -1), std::sregex_token_iterator()};
+    }
+    catch (...) {
+        handleException();
+    }
+    return {};
+}
+
+std::string RyRegularExpression::remove(const std::string& text) const
+{
+    return replace(text, "");
+}
+
+void RyRegularExpression::handleException()
+{
+    try {
+        throw;
+    }
+    catch (const std::regex_error& e) {
+        std::cerr << "Regex error: " << e.what() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown error" << std::endl;
+    }
+}
+} // namespace ry
---
+++ RyRegularExpresson.h
@@ -0,0 +1,69 @@
+﻿/**
+ * @author rayzhang
+ * @brief
+ * @date 2024-03-19
+ */
+#pragma once
+#include <regex>
+
+namespace ry
+{
+class RyRegularExpressionMatch
+{
+public:
+    RyRegularExpressionMatch(const std::smatch& matches);
+
+    // 返回捕获组的数量
+    size_t count() const;
+
+    // 返回指定捕获组的文本
+    std::string at(size_t index) const;
+    std::string captured(size_t index) const;
+    std::string operator[](size_t index) const;
+
+    // 返回所有捕获组的文本
+    std::vector<std::string> capturedTexts() const;
+
+    // 判断是否有匹配
+    bool hasMatch() const;
+
+private:
+    std::smatch m_matches;
+};
+
+class RyRegularExpression
+{
+public:
+    enum : int
+    {
+        CaseInsensitiveOption = std::regex_constants::icase,
+        ECMAScript            = std::regex_constants::ECMAScript
+    };
+
+    // 构造函数，初始化正则表达式
+    explicit RyRegularExpression(const std::string& pattern, int flags = std::regex_constants::ECMAScript); // 开启ECMAScript语法以及大小写不敏感选项
+
+    // 设置正则表达式模式
+    void setPattern(const std::string& pattern);
+
+    // 匹配函数，返回一个包含所有捕获组的vector
+    RyRegularExpressionMatch match(const std::string& text) const;
+
+    // 搜索函数，返回一个包含所有捕获组的vector
+    RyRegularExpressionMatch search(const std::string& text) const;
+
+    // 替换函数，返回替换后的字符串
+    std::string replace(const std::string& text, const std::string& replacement) const;
+
+    // 分割函数，返回分割后的字符串
+    std::vector<std::string> split(const std::string& text) const;
+
+    // 移除
+    std::string remove(const std::string& text) const;
+
+    static void handleException();
+
+private:
+    std::regex m_regex; // 存储正则表达式对象
+};
+} // namespace ry
